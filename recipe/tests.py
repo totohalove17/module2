4445e7b744c5aa8c2d78a5714de60e55392953d1
Anton <anton.babych35@knu.ua>
2025-05-23 14:30:44 +0300

    creation test

--- recipe/tests.py
+++ recipe/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Category, Recipe
 
 # Create your tests here.
+class CategoryModelTest(TestCase):
+    def setUp(self):
+        # Створення тестових даних
+        self.category = Category.objects.create(name="Desserts")
+
+    def test_category_creation(self):
+        """Тест для перевірки створення об'єкта Category"""
+        self.assertEqual(self.category.name, "Desserts")
+        self.assertIsInstance(self.category, Category)
+
+    
