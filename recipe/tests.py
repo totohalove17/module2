3a5e2014b0b6a5f87b0bc696b2aecbc4537eecf0
Данил Прохоренко <LymmyBar@gmail.com>
2025-05-23 14:31:18 +0300

    Added tests

--- recipe/tests.py
+++ recipe/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Recipe, Category
 
-# Create your tests here.
+
+class RecipeModelTest(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name="Десерти")
+
+    def test_create_recipe(self):
+        recipe = Recipe.objects.create(
+            title="Шарлотка",
+            description="Простий яблучний пиріг.",
+            instructions="Змішати інгредієнти та випікати 40 хвилин.",
+            ingredients="яблука, яйця, борошно, цукор",
+            category=self.category,
+        )
